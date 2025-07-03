0df5c7cee716b9fb3ed6f27cf41ff6d1f1066645
Dannylez <dlezama0796@gmail.com>
2025-07-03 13:40:56 -0300

    printflow project

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -2,17 +2,17 @@ import styles from './projects.module.css';
 import { Card } from './Card';
 
 export function Projects() {
-	const sportCenter = {
-		name: 'Sport Center',
+	const printFlow = {
+		name: 'PrintFlow',
 		description:
-			'Developed a comprehensive web application for	a gym using React, MongoDB, Redux, and other technologies. The platform primarily serves instructors by allowing them to manage student attendance, process payments, and handle class grid displaying class timings, locations, and attendee lists, facilitating efficient class management and real-time updates.',
+			'Developed a full-stack web application for a printing company using React, Node.js, Express, and MongoDB. The platform enables users to create and manage product orders, track production progress through a Kanban-style task board, and store relevant documents and files using AWS S3. Features include advanced filtering and pagination for handling large datasets, user authentication with JWT, and a responsive interface built with CSS Modules and Redux for state management.',
 		images: [
-			'/sport-center/image1.jpeg',
-			'/sport-center/image2.jpeg',
-			'/sport-center/image3.jpeg',
-			'/sport-center/image4.jpeg',
+			'/printFlow/image1.jpeg',
+			'/printFlow/image2.jpeg',
+			'/printFlow/image3.jpeg',
+			'/printFlow/image4.jpeg',
 		],
-		link: 'https://arena-sport-center-app.vercel.app/',
+		link: 'javascript:void(0)',
 	};
 	const printa = {
 		name: 'Printa',
@@ -26,6 +26,18 @@ export function Projects() {
 		],
 		link: 'https://printaimpresos.getprintbox.com/',
 	};
+	const sportCenter = {
+		name: 'Sport Center',
+		description:
+			'Developed a comprehensive web application for	a gym using React, MongoDB, Redux, and other technologies. The platform primarily serves instructors by allowing them to manage student attendance, process payments, and handle class grid displaying class timings, locations, and attendee lists, facilitating efficient class management and real-time updates.',
+		images: [
+			'/sport-center/image1.jpeg',
+			'/sport-center/image2.jpeg',
+			'/sport-center/image3.jpeg',
+			'/sport-center/image4.jpeg',
+		],
+		link: 'https://arena-sport-center-app.vercel.app/',
+	};
 
 	return (
 		<section className={styles.projects}>
@@ -34,8 +46,9 @@ export function Projects() {
 				<h3 className={styles.subTitle}>
 					Notable Projects
 				</h3>
-				<Card {...sportCenter} />
+				<Card {...printFlow} />
 				<Card {...printa} />
+				<Card {...sportCenter} />
 			</div>
 		</section>
 	);
